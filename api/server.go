90418e31ab22d09f9922b94812cd109300379fc4
Longs Pemun Gotar <developerlongs@gmail.com>
2024-06-01 08:56:44 +0100

    feat: implement support for graceful shutdown

--- api/server.go
+++ api/server.go
@@ -1,6 +1,14 @@
 package api
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/devlongs/ethereum-event-listener/api/handlers"
 	"github.com/devlongs/ethereum-event-listener/api/routes"
 	"github.com/devlongs/ethereum-event-listener/config"
@@ -16,17 +24,41 @@ type Server interface {
 type GinServer struct {
 	Engine *gin.Engine
 	DB     database.Database
+	Server *http.Server
 }
 
 func (s *GinServer) Start() error {
 	eventHandler := &handlers.EventHandler{DB: s.DB}
 	listenerHandler := &handlers.ListenerHandler{DB: s.DB}
 	routes.SetupRoutes(s.Engine, eventHandler, listenerHandler)
-	return s.Engine.Run(":8080")
+
+	s.Server = &http.Server{
+		Addr:    ":8080",
+		Handler: s.Engine,
+	}
+
+	go func() {
+		if err := s.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	return s.Stop()
 }
 
 func (s *GinServer) Stop() error {
-	// Implement graceful shutdown if necessary
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 	return nil
 }
 
